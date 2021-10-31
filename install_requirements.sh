a1dc03410fcd56a28ce6d46d2ce1406c534a0cf5
Kevin Deems <kevin8deems@gmail.com>
2021-10-31 17:51:26 +0000

    Updated go-react code

--- install_requirements.sh
+++ install_requirements.sh
@@ -5,4 +5,5 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-
 sudo apt-get update
 sudo apt-get -y install postgresql
 sudo service postgresql start
-sudo -u postgres createuser - superuser postgres
+
+# need to log in as postgres and alter the postgres password
